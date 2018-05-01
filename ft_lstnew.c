1cb7d36d3d0216d0918cfc95f025962d4c5478c4
Rebekah Astle <rastle@e1z3r5p24.42.us.org>
2018-05-01 12:40:16 -0700

    Starting on bonus list functions

---
+++ ft_lstnew.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rastle <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/01 12:18:52 by rastle            #+#    #+#             */
+/*   Updated: 2018/05/01 12:33:44 by rastle           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstnew(void const *content, size_t content_size)
+{
+	t_list *new;
+
+	new = (t_list *)malloc(*new);
+	if (!new)
+		return (NULL);
+	if (!content || !content_size)
+	{
+		new->content = NULL;
+		new->content_size = 0;
+	}
+	else
+	{
+		new->content = malloc(content_size);
+		if (!new->content)
+			return (NULL);
+		ft_memmove(new->content, content, content-size);
+		new->content_size = content_size;
+	}
+	new->next = NULL;
+	return (new);
+}
