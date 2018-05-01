a51804c5990f784cf3553bf66a724001283a2525
Rebekah Astle <rastle@e1z2r3p7.42.us.org>
2018-04-30 23:24:28 -0700

    Polishing main functions

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rastle <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/18 13:45:48 by rastle            #+#    #+#             */
-/*   Updated: 2018/04/30 20:19:02 by rastle           ###   ########.fr       */
+/*   Updated: 2018/04/30 23:09:38 by rastle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,34 @@
 
 char *ft_strtrim(char const *s)
 {
+	unsigned int a;
+	unsigned int b;
+	unsigned int c;
 	char *new;
-
-	new = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
-	if (new)
+	
+	a = 0;
+	c = 0;
+	if (!s)
+		return (NULL);
+	if ((int)(ft_strlen(s) - 1) == -1)
+		return (ft_strnew(1));
+	while ((s[a] == ' ' || s[a] == '\n' || s[a] == '\t'))
 	{
-		while (*s)
-		{
-			if (*s == ' ' || *s == '\n' || *s == '\t')
-			{
-				s++;
-			}
-			else
-			{
-				new = (char *)s;
-				s++;
-				new++;
-			}
-		}
-		*new = '\0';
-		return (new);
+		a++;
+		if (s[a] == '\0')
+			return (ft_strnew(1));
 	}
-	return (NULL);
+	b = ft_strlen(s) - 1;
+	while (s[b] == ' ' || s[b] == '\n' || s[b] == '\t')
+		b--;
+	new = (char *)malloc(sizeof(char) * (b - a + 2));
+	if (!new)
+		return (NULL);
+	while (c < b - a + 1)
+	{	
+		new[c] = s[a + c];
+		c++;
+	}
+	new[c] = '\0';
+	return (new);
 }
